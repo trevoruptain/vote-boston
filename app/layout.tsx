622a848cd170d61cc24c84ef923d9fc797dd3085
Trevor Uptain <trevor@gethyper.ai>
2025-01-26 16:45:19 -0500

    Fix name in title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Vote Boston",
+  title: "VoteBoston",
   description: "Find your polling location in Boston",
 };
 
